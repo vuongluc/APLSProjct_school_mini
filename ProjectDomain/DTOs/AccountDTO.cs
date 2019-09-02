3660f07c4271433574cb4e81cde88bf69e307d56
vuongluc <54711078+vuongluc@users.noreply.github.com>
2019-09-02 12:30:40 +0700

    Add files via upload

---
+++ ProjectDomain/DTOs/AccountDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDomain.DTOs
+{
+    public class AccountDTO
+    {
+        public string username { get; set; }
+
+        public string salf { get; set; }
+
+        public string password { get; set; }
+    }
+}
